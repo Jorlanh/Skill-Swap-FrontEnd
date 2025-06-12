adc94f94bfddd525a656b5a33b01924f8b228c7b
Jorlan Heider <jorlan25.js@gmail.com>
2025-06-12 06:56:37 -0300

    vai

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+// Caminho: src/environments/environment.prod.ts
+export const environment = {
+  production: true,
+  // Esta é a URL que seu frontend vai usar para se conectar ao backend
+  apiUrl: 'https://skillswap-backend-r7g8.onrender.com'
+};
